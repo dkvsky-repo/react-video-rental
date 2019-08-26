5c84652cb505b2f96863d295f99e49d8c8c37b1c
David Kontorovsky <1487550+programatiko@users.noreply.github.com>
2019-08-25 17:06:20 -0700

    Adding proptypes to Pagination component

--- src/components/common/Pagination.js
+++ src/components/common/Pagination.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Pagination = props => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
@@ -24,6 +25,13 @@ const Pagination = props => {
       </ul>
     </nav>
   );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
 };
 
 export default Pagination;
