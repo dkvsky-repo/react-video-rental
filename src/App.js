e0d81a50f68b30ae74042e368b48d43dadbfabff
David Kontorovsky <1487550+programatiko@users.noreply.github.com>
2019-09-13 07:18:54 -0700

    Adding a movie details page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/Movies';
+import MovieForm from './components/MovieForm';
 import Customers from './components/Customers';
 import Rentals from './components/Rentals';
 import NotFound from './components/NotFound';
@@ -13,6 +14,7 @@ const App = () => {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
